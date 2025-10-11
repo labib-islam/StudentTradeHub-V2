e13a128d2b00138c329c4a7160a7d3e04ca7f8d7
1mag1ne1 <yzhang25@mun.ca>
2025-10-11 21:28:21 -0230

    11-creating-users-in-the-backend

--- Web Application/backend/controllers/auth.controller.js
+++ Web Application/backend/controllers/auth.controller.js
@@ -1,10 +1,45 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcrypt"
+
 // Signup
 export const signup = async (req, res) => {
     try {
-        res.send('signup kk')
+        const { firstName, lastName, email, password } = req.body;
+        console.log(req.body);
+        // Check for empty fields
+        if ( !firstName|| !lastName || !email || !password) {
+        return res
+            .status(400)
+            .json({ message: "Please enter all required fields." });
+        }
+        
+        // Check if email already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+        return res.status(400).json({
+            message: "An account with this email exists.",
+        });
+        }
+
+        // hash the password
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // create and save new user to the DB
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+        });
+
+        const savedUser = await newUser.save();
+        
+        res.send({User: savedUser});
     } catch (err) {
-        console.error("test error msg")
+        console.error(err);
     }
+    
 }
 
 // Login
