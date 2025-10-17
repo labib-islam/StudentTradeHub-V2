a333faac5f5ced000dd666b1b95776a724c357c4
labib-islam <lislam@mun.ca>
2025-10-17 19:49:41 -0230

    21-implement-jwt-authentication

--- Web Application/backend/controllers/auth.controller.js
+++ Web Application/backend/controllers/auth.controller.js
@@ -1,63 +1,106 @@
-import User from "../models/user.model.js"
-import bcrypt from "bcrypt"
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // Signup
-export const signup = async (req, res) => {
-    try {
-        const { firstName, lastName, email, password } = req.body;
-        console.log(req.body);
-        // Check for empty fields
-        if ( !firstName|| !lastName || !email || !password) {
-        return res
-            .status(400)
-            .json({ message: "Please enter all required fields." });
-        }
-        
-        // Check if email already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-        return res.status(400).json({
-            message: "An account with this email exists.",
-        });
-        }
+const signup = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
 
-        // hash the password
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // create and save new user to the DB
-        const newUser = new User({
-            firstName,
-            lastName,
-            email,
-            password: hashedPassword,
-        });
-
-        const savedUser = await newUser.save();
-        
-        res.send({User: savedUser});
-    } catch (err) {
-        console.error(err);
+    // Check for empty fields
+    if (!firstName || !lastName || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please enter all required fields." });
     }
-    
-}
+
+    // Check if email already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "An account with this email exists.",
+      });
+    }
+
+    // hash the password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create and save new user to the DB
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+
+    // sign the token
+    const token = jwt.sign(
+      {
+        userId: savedUser._id,
+        userEmail: savedUser.email,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.send({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+};
 
 // Login
-export const login = async (req, res) => {
-    try {
-        res.send('login')
-    } catch (err) {
-        console.error("test error msg")
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check for empty fields
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please enter all required fields." });
     }
-}
+
+    // Check if user exists
+    const existingUser = await User.findOne({ email }).select("+password");
+    if (!existingUser)
+      return res.status(401).json({ message: "Wrong email or password" });
+
+    // Password Varification
+    const verifyPassword = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!verifyPassword)
+      return res.status(401).json({ message: "Wrong email or password" });
+
+    // sign the token
+    const token = jwt.sign(
+      {
+        userId: existingUser._id,
+        userEmail: existingUser.email,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+};
 
 // Logout
-export const logout = async (req, res) => {
-    try {
-        res.send('logout')
-    } catch (err) {
-        console.error("test error msg")
-    }
-}
+const logout = async (req, res) => {
+  try {
+    res.send("logout");
+  } catch (err) {
+    console.error("test error msg");
+  }
+};
 
 export default { signup, login, logout };
