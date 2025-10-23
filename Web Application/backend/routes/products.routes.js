041c74f0e49588d01f68bbb6b2c270271fbb5c35
labib-islam <lislam@mun.ca>
2025-10-22 22:04:03 -0230

    Implemented Product CRUD and Search Functionality using Controllers and Routes

---
+++ Web Application/backend/routes/products.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import fileUpload from "../middlewares/fileUpload.middleware.js";
+
+import productController from "../controllers/product.controller.js";
+import checkAuth from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.get("/", productController.getAllProducts);
+router.get("/:pid", checkAuth(), productController.getProductById);
+router.post(
+  "/new",
+  checkAuth("user"),
+  fileUpload.single("image"),
+  productController.createProduct
+);
+router.patch("/:pid", checkAuth("user"), productController.updateProduct);
+router.delete("/:pid", checkAuth("user"), productController.deleteProduct);
+
+export default router;
